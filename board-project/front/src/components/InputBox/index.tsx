5eb8ff0d6b9af4e4be3e2ef99d151f7765e95a20
Jinwoo Park <skin_lotion@naver.com>
2023-08-23 13:50:45 +0900

    practice 2023.08.23

--- board-project/front/src/components/InputBox/index.tsx
+++ board-project/front/src/components/InputBox/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import './style.css';
 
-export default function index() {
+//            interface : Input 상자 컴포넌트 Props           //
+interface Props {
+  label : string;
+  type : 'text' | 'password';
+  placeholder : string;
+  icon? : string;
+  errorMessage? : string;
+  onButtonClick? : () => void;
+}
+
+//            component : Input 상자 컴포넌트                 //
+export default function InputBox(props : Props) {
+
+//            state : Properties                              //
+    const {label, type, placeholder, icon, errorMessage} = props;
+    const {onButtonClick} = props;
+
+//            render : Input 상자 랜더링                        //
   return (
-    <div>index</div>
+    <div className = 'inputbox'>
+      <div className='inputbox-label'>{label}</div>
+      <div className={errorMessage ? 'inputbox-container-error' : 'inputbox-container'}>
+        <input className='input' type = {type} placeholder={placeholder}/>
+        { onButtonClick !== undefined && (
+           <div className='icon-button' onClick={onButtonClick}> 
+            {icon}
+            <div className='right-arrow-icon'></div>
+          </div>
+        )}
+      </div>
+      <div className='inputbox-message'>{errorMessage}</div>
+    </div>
   )
 }
