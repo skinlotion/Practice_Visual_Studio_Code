b9f5cd3a4b0a98bb85d17ba1396d1b145ee79da9
Jinwoo Park <skin_lotion@naver.com>
2023-08-29 11:52:54 +0900

    practice 2023.08.29

--- board-project/front/src/layouts/Footer/index.tsx
+++ board-project/front/src/layouts/Footer/index.tsx
@@ -4,10 +4,25 @@ import './style.css';
 //            component : 푸터 컴포넌트                //
 export default function Footer() {
 
+  //            event handler : 아이콘 버튼 클릭 이벤트 처리 함수             //
+  
+  // 기존 창에서 뜨는 이벤트
+  const onInstaIconClickHandler = () => {
+    window.location.href = 'https://www.instagram.com/';
+  }
+
+  // 새창으로 뜨는 이벤트
+  const onNaverIconClickHandler = () => {
+    window.open('https://blog.naver.com');
+  }
+  
+  
+
+
   //            render : 푸터 컴포넌트 랜더링            //
   return (
     <div id='footer'>
-      <div className='footer-top'>
+    <div className='footer-top'>
         <div className='footer-logo-box'>
           <div className='footer-logo-icon-box'>
             <div className='logo-right-icon'></div>
@@ -16,16 +31,16 @@ export default function Footer() {
         </div>
         <div className='footer-link-box'>
           <div className='email-link'>{'email@naver.com'}</div>
-          <div className='icon-button'>
+          <div className='icon-button' onClick={onInstaIconClickHandler}>
             <div className='insta-icon'></div>
           </div>
-          <div className='icon-button'>
+          <div className='icon-button' onClick={onNaverIconClickHandler}>
             <div className='naver-blog-icon'></div>
           </div>
         </div>
       </div>
       <div className='footer-bottom'>
-        <div className='footer=copyright'>{'Copyright © 2022 Jukoyakki. All Rights Reserved.'}</div>
+        <div className='footer-copyright'>{'Copyright © 2022 Jukoyakki. All Rights Reserved.'}</div>
       </div>
     </div>
   )
--- board-project/front/src/layouts/Header/index.tsx
+++ board-project/front/src/layouts/Header/index.tsx
@@ -1,8 +1,121 @@
-import React from 'react';
+import {useState, ChangeEvent, useEffect} from 'react';
 import './style.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AUTH_PATH, MAIN_PATH, BOARD_WRITE_PATH, SEARCH_PATH, BOARD_DETAIL_PATH, USER_PATH, BOARD_UPDATE_PATH } from 'constant';
+import { useCookies } from 'react-cookie';
 
+
+//            component : 헤더 컴포넌트                //
 export default function Header() {
+
+  //            state : path name 상태                  //
+  const { pathname } = useLocation();
+
+  //            state : cookie 상태                  //
+  const [cookies, setCookies] = useCookies();
+
+  //            variable : 인증 페이지 논리 변수            // 
+  const isAuthpage = pathname === AUTH_PATH;
+  //            variable : 메인 페이지 논리 변수            // 
+  const isMainPage = pathname === MAIN_PATH;
+  //            variable : 검색 페이지 논리 변수             // 
+  const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+  //            variable : 보드 디테일 페이지 논리 변수      // 
+  const isBoardDetailPage = pathname.startsWith(BOARD_DETAIL_PATH(''));
+  //            variable : 유저 페이지 논리 변수             // 
+  const isUserPage = pathname.startsWith(USER_PATH(''));
+  //            variable : 보드 작성 페이지 논리 변수        // 
+  const isBoardWritePage = pathname === BOARD_WRITE_PATH;
+  //            variable : 보드 수정 페이지 논리 변수       //  
+  const isBoardUpdatePage = pathname.startsWith(BOARD_UPDATE_PATH(''));
+
+  //            function : 네비게이트 함수             //
+  
+  const navigator = useNavigate();
+
+  //            event handler : 로고 클릭 이벤트 처리 함수             //
+  const onLogoClickHandler = () => {
+    navigator('/');
+  }
+  //            component : 검색 컴포넌트             //
+
+  const Search = () => {
+    //             event handler : 검색 값 변경 이벤트 처리            //
+
+    const onSearchValueCangeHadler = (event : ChangeEvent<HTMLInputElement>) => {
+      const searchValue = event.target.value;
+      setSearchValue(searchValue);
+    }
+
+    //             event handler : 검색 버튼 클릭 이벤트 처리          //
+    const onSearchButtonClickHandler = () => {
+      setShowInput(!showInput);
+    }
+
+
+    //            state : 검색 버튼 상태            //
+    const [showInput, setShowInput] = useState<Boolean>(false);
+    //            state : 검색 값 상태            //
+    const [searchValue, setSearchValue] = useState<string>('');
+
+  //            render : 검색 컴포넌트 랜더링            //
+  if(showInput)
+    return(
+    <div className='header-search-input-box'>
+      <input className='header-search-input' type='text' value={searchValue} onChange={onSearchValueCangeHadler}/>
+      <div className='icon-button' onClick={onSearchButtonClickHandler}>
+        <div className='search-icon'></div>
+      </div>
+    </div>  
+    );
+
+    return (
+      <div className='icon-button' onClick={onSearchButtonClickHandler}>
+        <div className='search-icon'></div>
+      </div>
+    );
+
+  };
+
+    //            component : 로그인 상태에 따라 로그인 혹은 마이페이지 버튼 컴포넌트             //
+    const LoginMyPageButton = () => {
+      if(cookies.email)
+      return (
+        <div className='mypage-button'>마이페이지</div>
+      )
+
+      return (
+        <div className='login-button'>로그인</div>
+      )
+    }
+
+    //            effect: 마운트시에만 실행될 함수          //
+    useEffect(() => {
+      setCookies('email', 'email@email.com', {path : '/'})
+      // setCookies('email', 'email@eamil.com', {path : '/' , expires : new Date() });
+    }, []);
+
+    //            render : 헤더 컴포넌트 랜더링            //
   return (
-    <div>Header</div>
+    <div id='header'>
+      <div className='header-container'>
+        <div className='header-left-box' onClick={onLogoClickHandler}>
+          <div className='header-logo-icon-box'>
+            <div className='logo-dark-icon'></div>
+          </div>
+          <div className='header-logo-text'>{'Hoons Board'}</div>
+        </div>
+        <div className='header-right-box'>
+          { isAuthpage && (<Search />) }
+          { isMainPage && (<> <Search /> <LoginMyPageButton/> </>)}
+          { isSearchPage && (<></>) }
+          { isBoardDetailPage && (<></>) }
+          { isUserPage && (<></>) }
+          { isBoardWritePage && (<></>) }
+          { isBoardUpdatePage && (<></>) }
+
+        </div>
+      </div>
+    </div>
   )
 }
